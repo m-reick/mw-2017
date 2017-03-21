8108b23d01a57194c9a482ccb60cd7a22a82e1bd
m-reick <michael.reick@yahoo.com>
2017-03-21 19:03:17 -0400

    Added minify and moderizr

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var minifycss = require ('gulp-minify-css');
 
 gulp.task('sass', function() {
   return gulp.src('src/scss/style.scss')
     .pipe(sass())
+    .pipe(minifycss())
     .pipe(gulp.dest('assets/css'));
 });
 
