4a5308cd5333fc14fe9356250be5b858497977a2
m-reick <michael.reick@yahoo.com>
2017-03-21 22:25:25 -0400

    Added error checking to gulp environment

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,36 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var minifycss = require ('gulp-minify-css');
+var browserSync = require('browser-sync').create();
+var plumber = require('gulp-plumber');
+var notifier = require('node-notifier');
+var notify = require('gulp-notify');
 
 gulp.task('sass', function() {
   return gulp.src('src/scss/style.scss')
-    .pipe(sass())
+    .pipe(plumber())
+    .pipe(sass()
+      .on("error", notify.onError({
+          title: "Sass Error",
+          message: "Error: <%= error.message %>"
+      }))
+    )
     .pipe(minifycss())
-    .pipe(gulp.dest('assets/css'));
+    .pipe(gulp.dest('assets/css'))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('default', function() {
+// BrowserSync server
+gulp.task('browser-sync', function() {
+    browserSync.init({
+       server: {
+            baseDir: "./"
+        }/* ,
+        proxy: "localhost:80"*/
+    });
+});
+
+gulp.task('default', ['sass', 'browser-sync'],function() {
   gulp.watch(['src/scss/*', 'src/scss/**/*'], ['sass']);
+  gulp.watch('*.html').on('change', browserSync.reload);
 });
